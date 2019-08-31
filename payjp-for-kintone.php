760f6f934dfe215590fbf2876bd7c1246555b6b1
tkc49 <h.tkc49@gmail.com>
2019-08-31 21:33:57 +0900

    Refactoring

--- payjp-for-kintone.php
+++ payjp-for-kintone.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:     PAY.JP for kintone
- * Plugin URI:      https://ht79.info
+ * Plugin URI:
  * Description:     This Plugin accept payments on your WordPress site via PAY.JP.
  * Author:          Takashi Hosoya
  * Author URI:      https://ht79.info
@@ -15,16 +15,20 @@
 
 define( 'PAY_JP_FOR_KINTONE_URL', plugins_url( '', __FILE__ ) );
 define( 'PAY_JP_FOR_KINTONE_PATH', dirname( __FILE__ ) );
-$data = get_file_data( __FILE__,
+$data = get_file_data(
+	__FILE__,
 	array(
 		'ver'   => 'Version',
 		'langs' => 'Domain Path',
-	) );
+	)
+);
 define( 'PAY_JP_FOR_KINTONE_VERSION', $data['ver'] );
 define( 'PAY_JP_FOR_KINTONE_LANGS', $data['langs'] );
-load_plugin_textdomain( 'payjp-for-kintone',
+load_plugin_textdomain(
+	'payjp-for-kintone',
 	false,
-	dirname( plugin_basename( __FILE__ ) ) . PAY_JP_FOR_KINTONE_LANGS );
+	dirname( plugin_basename( __FILE__ ) ) . PAY_JP_FOR_KINTONE_LANGS
+);
 
 require_once 'vendor/autoload.php';
 require_once dirname( __FILE__ ) . '/includes/class-payjp-for-kintone.php';
