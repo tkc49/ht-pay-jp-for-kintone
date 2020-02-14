c20b023e1aaeac098f9b80fe62d621f5d6169209
tkc49 <h.tkc49@gmail.com>
2020-02-14 16:44:12 +0900

    バージョン変更

--- ht-payjp-for-kintone.php
+++ ht-payjp-for-kintone.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:     HT PAY.JP for kintone PRO Beta Version 1
+ * Plugin Name:     HT PAY.JP for kintone PRO Beta Version 2.0.1
  * Plugin URI:
  * Description:     This Plugin accept payments on your WordPress site via PAY.JP.
  * Author:          Takashi Hosoya
